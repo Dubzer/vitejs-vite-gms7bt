6749fc25b9397494a6815cc7e7699a1fcbdd231f
Dubzer <dubzer@ya.ru>
2022-11-08 19:05:03 +0300

    1 file modified, 2 added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
+import { Button } from './ui/Button';
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
@@ -17,6 +17,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
+        <Button />
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -28,7 +29,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/ui/Button.tsx
@@ -0,0 +1,7 @@
+import classes from './Button.module.css';
+
+export function Button() {
+  return (
+    <button className={classes.button}>This button uses css module</button>
+  );
+}
